de59a8186ee1b0d9cfb8af894cef1dc904762da6
kwybro <kwybro@gmail.com>
2026-02-21 14:12:47 -0500

    Clean up

--- packages/data-ops/src/index.ts
+++ packages/data-ops/src/index.ts
@@ -1,12 +1,16 @@
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 
-export { eq, isNull } from "drizzle-orm";
+export { and, eq, getTableColumns, inArray, isNull } from "drizzle-orm";
 
 import * as appSchema from "./db/schema/app";
 import * as authSchema from "./db/schema/auth";
 
+export * from "./db/schema/app";
+export * from "./db/schema/auth";
+
 const schema = { ...appSchema, ...authSchema };
+
 export const createDb = (url: string, authToken?: string) => {
 	const client = createClient({ url, authToken });
 	return drizzle(client, { schema });
